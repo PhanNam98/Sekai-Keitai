54076db42f9c038869efecb2c41600697f6a2f18
PhanNam98 <phannamyoyo@gmail.com>
2018-11-24 23:13:56 +0700

    Fist

---
+++ QuanLyDienThoai/src/main/java/com/nnq/quanlydienthoai/QuanlydienthoaiApplication.java
@@ -0,0 +1,12 @@
+package com.nnq.quanlydienthoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlydienthoaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanlydienthoaiApplication.class, args);
+    }
+}
